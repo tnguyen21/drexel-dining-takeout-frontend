f7d59d034675b1fb06c436824b76a08c57a2e420
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-01-24 13:47:53 -0600

    add underline on hover for navbar links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <nav className="text-center mt-5">
-      <Link to="/" className="mx-2 font-semibold">
+      <Link to="/" className="mx-2 font-semibold hover:underline">
         Home
       </Link>
-      <Link to="/order" className="mx-2 font-semibold">
+      <Link to="/order" className="mx-2 font-semibold hover:underline">
         Order
       </Link>
-      <Link to="/service" className="mx-2 font-semibold">
+      <Link to="/service" className="mx-2 font-semibold hover:underline">
         Serve
       </Link>
     </nav>
