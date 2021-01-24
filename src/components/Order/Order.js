363c0897a8c0f164cb5728067f229dc8e9fac99a
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-01-24 16:47:05 -0600

    add firestore read/write functions

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,9 +3,27 @@ import firebase from "../Firestore/Firestore";
 
 export default function Order() {
   // Very bare bones of what the order page should look like
+  const db = firebase.firestore();
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+
+  function writerOrder() {
+    let order = db.collection("Orders").add({
+      status: 0,
+      student_name: "John Doe",
+      order_items: ["some", "item", "names"],
+      order_time: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    console.log("order sent!");
+  }
+
   return (
-    <h1 className="my-4 text-center text-2xl md:text-3xl lg:text-5xl font-black leading-tight">
-      Order
-    </h1>
+    <>
+      <h1 className="my-4 text-center text-2xl md:text-3xl lg:text-5xl font-black leading-tight">
+        Order
+      </h1>
+      <button onClick={() => writerOrder()}>Send Order!</button>
+    </>
   );
 }
