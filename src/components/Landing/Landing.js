c5b16a8128c7945714f61abe04f70bbbe7862895
Allen Cai <ac3978@drexel.edu>
2021-01-24 21:10:04 -0500

    Test, change dine and dash to grab and go

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,7 @@ export default function Landing() {
       <div class="container mx-auto h-screen">
         <div class="text-center px-3 lg:px-0">
           <h1 class="my-4 text-2xl md:text-3xl lg:text-5xl font-black leading-tight">
-            🐉 Dine and Dash
+            🐉 Grab and Go
           </h1>
           <p class="leading-normal text-gray-800 text-base md:text-xl lg:text-2xl mb-8">
             Skip the wait lines at Urban!
